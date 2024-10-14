c40d7aafa1df25a28bdf28ac27ca7fbe5ccd359c
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2024-10-14 20:19:59 -0300

    Deleting Old Dotfiles

--- olddotfiles/.config/nvim/lua/config/options.lua
+++
@@ -1,2 +0,0 @@
-local opt = vim.opt
-opt.wrap = true
--- olddotfiles/.config/nvim/lua/plugins/live-server.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "aurum77/live-server.nvim",
-  run = function()
-    require("live_server.util").install()
-  end,
-  cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
-}
