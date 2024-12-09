47a987c31f85caf5a6620ed57814a18c5a1bcee0
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2024-12-09 00:00:16 -0300

    Update .config/hypr/scripts/wallpaper.sh

--- dot_config/hypr/scripts/executable_wallpaper.sh
+++ dot_config/hypr/scripts/executable_wallpaper.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 # Set the path to the wallpapers directory
-wallpapersDir="$HOME/Imagens/wallpapers/"
+wallpapersDir="$HOME/.config/wallpapers/"
 
 # Get a list of all image files in the wallpapers directory
 wallpapers=("$wallpapersDir"/*)
 
 # Start an infinite loop
 while true; do
-    # Check if the wallpapers array is empty
-    if [ ${#wallpapers[@]} -eq 0 ]; then
-        # If the array is empty, refill it with the image files
-        wallpapers=("$wallpapersDir"/*)
-    fi
+  # Check if the wallpapers array is empty
+  if [ ${#wallpapers[@]} -eq 0 ]; then
+    # If the array is empty, refill it with the image files
+    wallpapers=("$wallpapersDir"/*)
+  fi
 
-    # Select a random wallpaper from the array
-    wallpaperIndex=$(( RANDOM % ${#wallpapers[@]} ))
-    selectedWallpaper="${wallpapers[$wallpaperIndex]}"
+  # Select a random wallpaper from the array
+  wallpaperIndex=$((RANDOM % ${#wallpapers[@]}))
+  selectedWallpaper="${wallpapers[$wallpaperIndex]}"
 
-    # Update the wallpaper using the swww img command
-    swww img "$selectedWallpaper" --transition-type wave --transition-step 10 --transition-fps 60 
+  # Update the wallpaper using the swww img command
+  swww img "$selectedWallpaper" --transition-type wave --transition-step 10 --transition-fps 60
   # Delay before selecting the next wallpaper
-    sleep 6h
+  sleep 6h
 done
+
