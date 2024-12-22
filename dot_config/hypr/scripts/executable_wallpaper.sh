1dd9d598d397c0ad9ceffb8a7895ed6ba8dfb7a7
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2024-12-22 10:13:46 -0300

    Update .config/hypr/scripts/wallpaper.sh Update .config/waybar/config.jsonc Update .config/waybar/style.css

--- dot_config/hypr/scripts/executable_wallpaper.sh
+++ dot_config/hypr/scripts/executable_wallpaper.sh
@@ -8,19 +8,18 @@ wallpapers=("$wallpapersDir"/*)
 
 # Start an infinite loop
 while true; do
-  # Check if the wallpapers array is empty
-  if [ ${#wallpapers[@]} -eq 0 ]; then
-    # If the array is empty, refill it with the image files
-    wallpapers=("$wallpapersDir"/*)
-  fi
+	# Check if the wallpapers array is empty
+	if [ ${#wallpapers[@]} -eq 0 ]; then
+		# If the array is empty, refill it with the image files
+		wallpapers=("$wallpapersDir"/*)
+	fi
 
-  # Select a random wallpaper from the array
-  wallpaperIndex=$((RANDOM % ${#wallpapers[@]}))
-  selectedWallpaper="${wallpapers[$wallpaperIndex]}"
+	# Select a random wallpaper from the array
+	wallpaperIndex=$((RANDOM % ${#wallpapers[@]}))
+	selectedWallpaper="${wallpapers[$wallpaperIndex]}"
 
-  # Update the wallpaper using the swww img command
-  swww img "$selectedWallpaper" --transition-type wave --transition-step 10 --transition-fps 60
-  # Delay before selecting the next wallpaper
-  sleep 6h
+	# Update the wallpaper using the swww img command
+	swww img "$selectedWallpaper" --transition-type wave --transition-step 60 --transition-fps 60
+	# Delay before selecting the next wallpaper
+	sleep 6h
 done
-
